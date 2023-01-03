cfafa45e39b69418570e1aa253c15585abe840fe
Geetha Medisetty <geetha.medisetty@gmail.com>
2023-01-03 00:06:21 -0800

    Best Time to Buy and Sell Stock Problem

--- bestTimeToSellStock.py
+++ bestTimeToSellStock.py
@@ -11,7 +11,9 @@ Return the maximum profit you can achieve from this transaction. If you cannot a
 """
 class Solution(object):
     """
-    Algorithm: Method 1(Time Limit Exceeded)
+    Algorithm: 
+    
+    Method 1(Time Limit Exceeded)
     1. Initialise a variable to hold maxProfit.
     2. Iterates through the prices array: from 0 to len prices array
     3. Initialise j= i+1, Iterate through prices array from i+1 to len of array
@@ -19,6 +21,17 @@ class Solution(object):
     5. return maxProfit
 
     Time Complexity: O(n*2)
+    Space Complexity: O(1)
+
+    Method 2: Sliding Window
+    1. Initialise a variable to hold maxProfit.
+    2. Initialise left to 0
+    3. Iterate right through the length of prices array from index 1
+    4. if the prices[right] < prices[left], update the left value to right
+    5. calculate the profit i.e prices[j] - prices[i]: maxProfit would be max of maxProfit and calculated profit
+    6. return maxProfit
+
+    Time Complexity: O(n)
     Space Complexity: O(1)
     """
     def maxProfit(self, prices):
@@ -28,9 +41,10 @@ class Solution(object):
         """
 
         maxProfit = 0
-        for i in range(len(prices)):
-            j = i+1
-            for j in range(j,len(prices)):
-                maxProfit = max(maxProfit, prices[j]-prices[i])
+        left = 0
+        for right in range(1, len(prices)):
+            if prices[right] < prices[left]:
+                left = right
+            maxProfit = max(maxProfit, prices[right] - prices[left])
 
         return maxProfit
